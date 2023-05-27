afa276410e12a04f8adb813b49e8642b29343589
Sam-Krish17 <126261435+Sam-Krish17@users.noreply.github.com>
2023-05-27 23:48:44 +0530

    New Portfolio Site
    Added some component

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ResumeComponent } from './resume/resume.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+{path:'resume',component:ResumeComponent},
+{path:'home',component:HomeComponent},
+{path: '', redirectTo: 'home', pathMatch: 'full'},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ResumeComponent } from './resume/resume.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ResumeComponent,
+    ProjectsComponent,
+    SkillsComponent,
+    ContactsComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
